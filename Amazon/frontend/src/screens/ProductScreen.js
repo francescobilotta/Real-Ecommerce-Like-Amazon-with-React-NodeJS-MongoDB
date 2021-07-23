2bbd939bf7e29720e249f49a900f27d03f23ce4d
Francesco Bilotta <bilottafrancesco.business@gmail.com>
2021-07-23 18:46:28 +0200

    Product page layout bug fixed

--- Amazon/frontend/src/screens/ProductScreen.js
+++ Amazon/frontend/src/screens/ProductScreen.js
@@ -26,69 +26,66 @@ export default function ProductScreen(props) {
                 <MessageBox variant="danger">{error}</MessageBox>
             ) : (
                 <div>
-                    <Link to="/">Back to Home</Link>
-                    <div className="row center">
-                        <div className="row top">
-                            <div className="col-2">
-                                <img
-                                    className="large"
-                                    src={product.image}
-                                    alt={product.name}
-                                ></img>
-                            </div>
-                            <div className="col-1">
+                    <Link to="/">Back to result</Link>
+                    <div className="row top">
+                        <div className="col-2">
+                            <img
+                                className="large"
+                                src={product.image}
+                                alt={product.name}
+                            ></img>
+                        </div>
+                        <div className="col-1">
+                            <ul>
+                                <li>
+                                    <h1>{product.name}</h1>
+                                </li>
+                                <li>
+                                    <Rating
+                                        rating={product.rating}
+                                        numReviews={product.numReviews}
+                                    ></Rating>
+                                </li>
+                                <li>Pirce : ${product.price}</li>
+                                <li>
+                                    Description:
+                                    <p>{product.description}</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-1">
+                            <div className="card card-body">
                                 <ul>
                                     <li>
-                                        <h1>{product.name}</h1>
+                                        <div className="row">
+                                            <div>Price</div>
+                                            <div className="price">
+                                                ${product.price}
+                                            </div>
+                                        </div>
                                     </li>
                                     <li>
-                                        <Rating
-                                            rating={product.rating}
-                                            numReviews={product.numReviews}
-                                        ></Rating>
+                                        <div className="row">
+                                            <div>Status</div>
+                                            <div>
+                                                {product.countInStock > 0 ? (
+                                                    <span className="success">
+                                                        In Stock
+                                                    </span>
+                                                ) : (
+                                                    <span className="danger">
+                                                        Unavailable
+                                                    </span>
+                                                )}
+                                            </div>
+                                        </div>
                                     </li>
-                                    <li>Price: ${product.price}</li>
                                     <li>
-                                        Description:
-                                        <p>{product.description}</p>
+                                        <button className="primary block">
+                                            Add to Cart
+                                        </button>
                                     </li>
                                 </ul>
-                            </div>
-                            <div className="col-1">
-                                <div className="card card-body">
-                                    <ul>
-                                        <li>
-                                            <div className="row">
-                                                <div>Price</div>
-                                                <div className="price">
-                                                    ${product.price}
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div className="row">
-                                                <div>Status</div>
-                                                <div>
-                                                    {product.countInStock >
-                                                    0 ? (
-                                                        <span className="success">
-                                                            In Stock
-                                                        </span>
-                                                    ) : (
-                                                        <span className="danger">
-                                                            Unavailable
-                                                        </span>
-                                                    )}
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <button className="primary block">
-                                                Add to Cart
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </div>
                             </div>
                         </div>
                     </div>
